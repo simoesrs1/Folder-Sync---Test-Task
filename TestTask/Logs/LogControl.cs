f2afd31b9df67c1a0570ea6e45d18e37e7421a22
simoesrs1 <rubensimoes2001.rse@gmail.com>
2024-06-27 19:28:18 +0100

    added log file

---
+++ TestTask/Logs/LogControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Logs
+{
+    public static class LogControl
+    {
+        private static string LogPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" + "log.txt";
+        public static void WriteLog(string logMessage)
+        {
+            try
+            {
+                FileInfo logFileInfo = new FileInfo(LogPath);
+                DirectoryInfo logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+                if (!logDirInfo.Exists)
+                    logDirInfo.Create();
+                using (FileStream fileStream = new FileStream(LogPath, FileMode.Append))
+                {
+                    using (StreamWriter log = new StreamWriter(fileStream))
+                    {
+                        log.WriteLine(logMessage);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
